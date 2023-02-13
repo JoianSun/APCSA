2ee36a5976219ca387897d074a6c7e1e4d671784
JoianSun <13290140+JoianSun@users.noreply.github.com>
2023-02-13 22:34:12 +0800

    Create 101-PrimeFinder.java

---
+++ FRQ/101-PrimeFinder.java
@@ -0,0 +1,34 @@
+public class PrimeFinder
+{
+
+  /** Returns the difference between num and the closest prime number which
+   *  is greater than or equal to num
+   *  Precondition: num is positive
+   */
+  public static int gapToNextPrime(int num)
+  {
+    /* Implement your answer to part (b) here */
+    int gap = 0;
+    while (!isPrime(num + gap)) {
+      gap++;
+    }
+    return gap;
+
+  }
+
+  /** Returns true if the integer n is a prime number
+   *  Precondition: n is positive
+   */
+  private static boolean isPrime(int n)
+  {
+    /* COMPLETE WORKING METHOD PROVIDED */
+    /* DO NOT MODIFY THE IMPLEMENTATION OF THIS METHOD*/
+    if(n <= 1) return false;
+    if(n==2) return true;
+    for(int i = 2; i < Math.sqrt(n)+1; i++) if(n%i == 0) return false;
+    return true;
+  }
+
+  /* Methods for previous parts of this question are not shown */
+
+}
